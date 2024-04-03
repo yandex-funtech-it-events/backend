804a7fa67f8149b73e26d658b4a751a89dc6ca76
kbobkov <deltabobkov>
2024-04-03 18:31:37 +0300

    Users model and admin v1

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    """Управление пользователями"""
+    list_display = (
+        "id",
+        "role",
+        "username",
+        "email",
+        "first_name",
+        "last_name"
+    )
+    list_display_links = ("id", "email")
+    search_fields = ("email", "role")
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from core.models import TimeStamp
+from core.constants import FieldLenght
 
-# Create your models here.
+
+class MyUser(AbstractUser, TimeStamp):
+    """
+    Кастомная модель переопределенного юзера.
+    При создании пользователя все поля обязательны для заполнения.
+    """
+    class RoleChoises(models.TextChoices):
+        """
+        Определение роли юзера.
+        """
+        ATTENDEE = "attendee"
+        ORGANIZER = "organizer"
+        ADMIN = "admin"
+
+    email = models.EmailField(
+        "Почта",
+        unique=True,
+        max_length=FieldLenght.MAX_LENGHT_EMAIL.value,
+    )
+    password = models.CharField(
+        "Пароль пользователя",
+        max_length=FieldLenght.MAX_LENGHT_PASSWORD.value,
+    )
+    first_name = models.CharField(
+        "Имя пользователя",
+        max_length=FieldLenght.MAX_LENGHT_FIRST_NAME.value,
+    )
+    last_name = models.CharField(
+        "Фамилия пользователя",
+        max_length=FieldLenght.MAX_LENGHT_LAST_NAME.value,
+    )
+    role = models.TextField(
+        "Пользовательская роль",
+        choices=RoleChoises.choices,
+        default=RoleChoises.ATTENDEE,
+        max_length=FieldLenght.MAX_LENGHT_ROLE.value,
+    )
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["-id"]
+
+    def __str__(self):
+        return str(self.email)
