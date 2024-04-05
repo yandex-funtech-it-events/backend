14041aded33b96c7fc0041d9b3f7b437544c5243
maria.subbotina <maria.subbotina@aramcoinnovations.com>
2024-04-05 11:00:48 +0300

    Registration draft

--- apps/events/models.py
+++ apps/events/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+from apps.users.models import CustomUser
+from apps.core.constants import MAX_LENGTH_REGISTRATION_STAGE
+
+
+class RegistrationStageChoices(models.TextChoices):
+    """Стадия регистрации на мероприятие"""
+    PARTICIPATED = ("participated", "Участвовал")
+    PARTICIPATING = ("participating", "Участвую")
+    NOT_REGISTERED = ("Not_registered", "Не зарегистрирован")
+
+
+class Registration(models.Model):
+    """Модель регистрации на мероприятие"""
+
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+        verbose_name="мероприятие",
+        related_name="registrated_event"
+    )
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        verbose_name="участник",
+        related_name="participants"
+    )
+    # notification = models.ForeignKey()
+    registration_stage = models.CharField(
+        "стадия регистрации",
+        choices=RegistrationStageChoices.choices,
+        default=RegistrationStageChoices.NOT_REGISTERED,
+        max_length=MAX_LENGTH_REGISTRATION_STAGE
+    )
+
+    class Meta:
+        verbose_name = "Регистрация"
+        verbose_name_plural = "Регистрации"
--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from apps.core.constants import MAX_LENGTH_NOTIFICATION_TEXT
+
+
+# class Notification(models.Model):
+#     """Модель уведомления"""
+#     text = models.CharField(
+#         "текст уведомления",
+#         max_length=MAX_LENGTH_NOTIFICATION_TEXT
+#     )
+#     date_popup = models.DateTimeField(
+#         "дата оповещения", blank=False, null=False
+#     )
+#     qrcode_ticket = models.ImageField(
+#         "qr код для мероприятия",
+#         blank=True, upload_to="qr_codes/"
+#     )
