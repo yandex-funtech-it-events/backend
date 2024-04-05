5ed354a603e1d010d09a44fa04bf1eae44bee914
анастасия <hutjicsgo@gmail.com>
2024-04-05 10:42:10 +0300

    feat/events: fixed timestamp

--- apps/core/models.py
+++ apps/core/models.py
@@ -15,18 +15,12 @@ class EventTimeStamp(models.Model):
     """Модель временных меток для events"""
 
     created_at = models.DateTimeField("дата создания", auto_now_add=True, db_index=True)
-    registration_open = models.DateTimeField(
-        "дата открытия регистрации", blank=True, null=True, db_index=True
-    )
+    registration_open = models.DateTimeField("дата открытия регистрации", db_index=True)
     registration_close = models.DateTimeField(
-        "дата закрытия регистрации", blank=True, null=True, db_index=True
+        "дата закрытия регистрации", db_index=True
     )
-    date_start = models.DateField(
-        "дата начала мероприятия", blank=True, null=True, db_index=True
-    )
-    date_end = models.DateField(
-        "дата окончания мероприятия", blank=True, null=True, db_index=True
-    )
+    date_start = models.DateField("дата начала мероприятия", db_index=True)
+    date_end = models.DateField("дата окончания мероприятия", db_index=True)
 
     class Meta:
         abstract = True
