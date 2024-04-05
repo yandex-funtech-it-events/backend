de9daefa61b33a04877b8e373330e3581157edc3
kbobkov <deltabobkov>
2024-04-05 17:10:23 +0300

    Users models with info, settings and filters

--- apps/core/models.py
+++ apps/core/models.py
@@ -15,12 +15,18 @@ class EventTimeStamp(models.Model):
     """Модель временных меток для events"""
 
     created_at = models.DateTimeField("дата создания", auto_now_add=True, db_index=True)
-    registration_open = models.DateTimeField("дата открытия регистрации", db_index=True)
-    registration_close = models.DateTimeField(
-        "дата закрытия регистрации", db_index=True
+    registration_open = models.DateTimeField(
+        "дата открытия регистрации", default=None, db_index=True
     )
-    date_start = models.DateField("дата начала мероприятия", db_index=True)
-    date_end = models.DateField("дата окончания мероприятия", db_index=True)
+    registration_close = models.DateTimeField(
+        "дата закрытия регистрации", default=None, db_index=True
+    )
+    date_start = models.DateField(
+        "дата начала мероприятия", default=None, db_index=True
+    )
+    date_end = models.DateField(
+        "дата окончания мероприятия", default=None, db_index=True
+    )
 
     class Meta:
         abstract = True
--- apps/users/models.py
+++ apps/users/models.py
@@ -17,6 +17,7 @@ class CustomUser(AbstractUser, TimeStamp):
     """
     Кастомная модель переопределенного юзера.
     При создании пользователя все поля обязательны для заполнения.
+    По логике эти данные будут подтягиваться из Яндекс ID.
     """
 
     email = models.EmailField(
@@ -36,6 +37,10 @@ class CustomUser(AbstractUser, TimeStamp):
         "Фамилия пользователя",
         max_length=FieldLength.MAX_LENGTH_LAST_NAME.value,
     )
+    phone = models.CharField(
+        "Номер телефона пользователя",
+        max_length=FieldLength.MAX_LENGTH_PHONE.value,
+    )
     role = models.TextField(
         "Пользовательская роль",
         choices=RoleChoices.choices,
@@ -50,3 +55,188 @@ class CustomUser(AbstractUser, TimeStamp):
 
     def __str__(self):
         return str(self.email)
+
+
+class ExperienceChoices(models.TextChoices):
+    """Указание опыта юзера."""
+
+    LESS_1 = ("0-1", "0-1 год")
+    MORE_1_LESS_3 = ("1-3", "1-3 года")
+    MORE_3_LESS_5 = ("3-5", "3-5 лет")
+    MORE_5_LESS_10 = ("5-10", "5-10 лет")
+    MORE_10 = ("10+", "Больше 10 лет")
+
+
+class Specialization(models.Model):
+    """Модель для поля направления работы.
+
+    Описывается следующими полями:
+    name - Название направления работы.
+    slug - слаг направления работы.
+    """
+
+    name = models.CharField(
+        "Название",
+        unique=True,
+        max_length=FieldLength.MAX_NAME.value,
+    )
+    slug = models.SlugField(
+        "Уникальный слаг",
+        unique=True,
+        max_length=FieldLength.MAX_SLUG.value,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Направление работы"
+        verbose_name_plural = "Направления работы"
+        ordering = ["name"]
+
+
+class CustomUserInfo(models.Model):
+    """
+    Модель для дополнительной информации о пользователе.
+    При регистрации личного кабинета все поля обязательны для заполнения.
+    """
+
+    user = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name="info",
+        verbose_name="Пользователь",
+    )
+    image = models.ImageField(
+        "Фото",
+        upload_to="users/images/",
+        null=True,
+        default=None,
+        blank=True,
+    )
+    job = models.CharField(
+        "Место работы",
+        max_length=FieldLength.MAX_LENGTH_JOB.value,
+    )
+    experience = models.TextField(
+        "Опыт работы",
+        choices=ExperienceChoices.choices,
+        default=ExperienceChoices.LESS_1,
+        max_length=FieldLength.MAX_LENGTH_EXPERIENCE.value,
+    )
+    specialization = models.ForeignKey(
+        Specialization,
+        related_name="users_info",
+        on_delete=models.CASCADE,
+        verbose_name="Направление работы",
+        blank=True,
+        null=True,
+    )
+    user_agree_personal_info = models.BooleanField(
+        "Согласие на передачу данных",
+        default=False,
+    )
+    user_agree_publish_cv = models.BooleanField(
+        "Согласие на публикацию резюме",
+        default=False,
+    )
+
+    class Meta:
+        verbose_name = "Доп. инфо о пользователе"
+        verbose_name_plural = "Доп. инфо о пользователях"
+        ordering = ["-pk"]
+
+    def __str__(self):
+        return str(self.pk)
+
+
+class CustomUserSettings(models.Model):
+    """
+    Модель для настроек пользователя в личном кабинете.
+    """
+
+    user = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name="settings",
+        verbose_name="Пользователь",
+    )
+    ev_notification_start = models.BooleanField(
+        "Уведомлять о начале событий",
+        default=False,
+    )
+    ev_notification_new = models.BooleanField(
+        "Уведомлять о новых событиях",
+        default=False,
+    )
+
+    class Meta:
+        verbose_name = "Настройки пользователя"
+        verbose_name_plural = "Настройки пользователей"
+        ordering = ["-pk"]
+
+    def __str__(self):
+        return str(self.pk)
+
+
+class CustomUserFilter(models.Model):
+    """
+    Модель для фильтра пользователя в личном кабинете.
+    """
+
+    user = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name="filter",
+        verbose_name="Пользователь",
+    )
+    is_ev_online = models.BooleanField(
+        "Онлайн событие",
+        default=False,
+    )
+    city = models.CharField(
+        "Город проведения",
+        max_length=FieldLength.MAX_LENGTH_CITY.value,
+        blank=True,
+        null=True,
+    )
+    specialization = models.ManyToManyField(
+        Specialization,
+        related_name="users_filter",
+        verbose_name="Направление работы",
+        blank=True,
+        through="users.SpecializationsInFilter",
+    )
+
+    class Meta:
+        verbose_name = "Фильтр пользователя"
+        verbose_name_plural = "Фильтры пользователей"
+        ordering = ["-pk"]
+
+    def __str__(self):
+        return str(self.pk)
+
+
+class SpecializationsInFilter(models.Model):
+    """
+    Направления работы в фильтре у пользовтеля.
+    У одного пользователя может быть 1 и более направлений.
+    """
+
+    filter = models.ForeignKey(
+        CustomUserFilter,
+        on_delete=models.CASCADE,
+        related_name="specs_in_filter",
+        verbose_name="Фильтр",
+    )
+    specialization = models.ForeignKey(
+        Specialization, on_delete=models.CASCADE, verbose_name="Направления в фильтре."
+    )
+
+    class Meta:
+        verbose_name = "Направления работы в фильтре"
+        verbose_name_plural = "Направления работы в фильтрах"
+        ordering = ["-pk"]
+
+    def __str__(self):
+        return f"{self.filter.user.email}"
