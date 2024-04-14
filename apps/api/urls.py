bc9061bfa68feb42f35495266727d613d8afcef5
анастасия <hutjicsgo@gmail.com>
2024-04-15 00:39:15 +0300

    feat/hotfix

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,24 +1,8 @@
 from django.urls import include, path
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView,
-)
 
 app_name = "apps.api"
 
 urlpatterns = [
     path("v1/", include("apps.api.v1.urls")),
-    path("", include("djoser.urls")),
-    path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "schema/swagger/",
-        SpectacularSwaggerView.as_view(url_name="apps.api:schema"),
-        name="swagger-ui",
-    ),
-    path(
-        "schema/redoc/",
-        SpectacularRedocView.as_view(url_name="apps.api:schema"),
-        name="redoc",
-    ),
+    path("v1/", include("djoser.urls")),
 ]
