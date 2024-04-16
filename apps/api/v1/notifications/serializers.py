a21d48a299012c3b25fac74549a688ef6cd2ecdd
анастасия <hutjicsgo@gmail.com>
2024-04-16 16:04:10 +0300

    feat/workflows

--- apps/api/v1/notifications/serializers.py
+++ apps/api/v1/notifications/serializers.py
@@ -1,9 +1,9 @@
 import datetime
-import qrcode
 import os
 
-from django.shortcuts import get_object_or_404
+import qrcode
 from django.conf import settings
+from django.shortcuts import get_object_or_404
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
@@ -15,11 +15,7 @@ NOTIFICATION_TIME = datetime.time(10, 0)
 
 def generate_qr(data_to_encode: str):
     qr_img = qrcode.make(data_to_encode)
-    path = os.path.join(
-        settings.MEDIA_ROOT,
-        "qr_codes",
-        f"{data_to_encode}.jpg"
-    )
+    path = os.path.join(settings.MEDIA_ROOT, "qr_codes", f"{data_to_encode}.jpg")
     qr_img.save(path)
     return f"{data_to_encode}.jpg"
 
@@ -38,26 +34,20 @@ class NotificationSerializer(serializers.ModelSerializer):
             "notification_at",
             "ticket_qr_code",
         )
-        extra_kwargs = {'ticket_qr_code': {'required': False}}
+        extra_kwargs = {"ticket_qr_code": {"required": False}}
 
     def create(self, validated_data):
-        event = get_object_or_404(
-            Events,
-            id=self.context.get("event_id")
-        )
-        user = self.context.get('request').user
-        notification_at = datetime.datetime.combine(
-            event.date_start,
-            NOTIFICATION_TIME
-        )
-        print('creating_qr')
-        qr_img_path = generate_qr(f'event_{event.id}_user_{user.id}')
+        event = get_object_or_404(Events, id=self.context.get("event_id"))
+        user = self.context.get("request").user
+        notification_at = datetime.datetime.combine(event.date_start, NOTIFICATION_TIME)
+        print("creating_qr")
+        qr_img_path = generate_qr(f"event_{event.id}_user_{user.id}")
 
         notification = Notification.objects.create(
             event=event,
             user=user,
             notification_at=notification_at,
             ticket_qr_code=qr_img_path,
-            **validated_data
+            **validated_data,
         )
         return notification
